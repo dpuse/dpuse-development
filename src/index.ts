10c7b73c1fb1665247a94297c1b213c357b585e9
Jonathan Terrell <terrell.jm@gmail.com>
2026-04-29 17:53:47 +0200

    v0.3.498

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,69 @@
-/* eslint-disable security/detect-non-literal-fs-filename */
-
 // External Dependencies
-import { promises as fs } from 'node:fs';
-import { nanoid } from 'nanoid';
+// import { promises as fs } from 'node:fs';
+// import { nanoid } from 'nanoid';
 
-// Interfaces/Types
-interface DirectoryEntry {
-    name: string;
-    typeId: 'folder' | 'object';
-}
-interface DirectoryFolderEntry extends DirectoryEntry {
-    childCount: number;
-    typeId: 'folder';
-}
-interface DirectoryObjectEntry extends DirectoryEntry {
-    id: string;
-    lastModifiedAt: number;
-    size: number;
-    typeId: 'object';
-}
+// Types ───────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+// interface DirectoryEntry {
+//     name: string;
+//     typeId: 'folder' | 'object';
+// }
+
+// interface DirectoryFolderEntry extends DirectoryEntry {
+//     childCount: number;
+//     typeId: 'folder';
+// }
+
+// interface DirectoryObjectEntry extends DirectoryEntry {
+//     id: string;
+//     lastModifiedAt: number;
+//     size: number;
+//     typeId: 'object';
+// }
 
 // Actions ─────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
-export async function buildDirectoryIndex(id: string): Promise<void> {
-    try {
-        console.info(`🚀 Building public directory index for identifier '${id}'...`);
-        const index: Record<string, DirectoryEntry[]> = {};
+// export async function buildDirectoryIndex(id: string, outputDirectory = 'public'): Promise<void> {
+//     try {
+//         console.info(`🚀 Building directory index for identifier '${id}'...`);
+//         const index: Record<string, DirectoryEntry[]> = {};
 
-        async function listDirectoryEntriesRecursively(directoryPath: string, names: string[]): Promise<void> {
-            console.info(`⚙️ Processing directory '${directoryPath}'...`);
-            const entries: DirectoryEntry[] = [];
-            const localDirectoryPath = directoryPath.slice(`public/${id}`.length);
-            index[localDirectoryPath === '' ? '/' : localDirectoryPath] = entries;
-            for (const name of names) {
-                const itemPath = `${directoryPath}/${name}`;
-                try {
-                    const stats = await fs.stat(itemPath);
-                    if (stats.isDirectory()) {
-                        const nextLevelChildren = await fs.readdir(itemPath);
-                        const folderEntry: DirectoryFolderEntry = { childCount: nextLevelChildren.length, name, typeId: 'folder' };
-                        entries.push(folderEntry);
-                        await listDirectoryEntriesRecursively(itemPath, nextLevelChildren);
-                    } else {
-                        const objectEntry: DirectoryObjectEntry = { id: nanoid(), lastModifiedAt: stats.mtimeMs, name, size: stats.size, typeId: 'object' };
-                        entries.push(objectEntry);
-                    }
-                } catch (error) {
-                    throw new Error(`Unable to get information for '${name}' in 'buildPublicDirectoryIndex'. ${String(error)}`);
-                }
-            }
-            entries.sort((left, right) => {
-                const typeComparison = left.typeId.localeCompare(right.typeId);
-                return typeComparison === 0 ? left.name.localeCompare(right.name) : typeComparison;
-            });
-        }
+//         async function listDirectoryEntriesRecursively(directoryPath: string, names: string[]): Promise<void> {
+//             console.info(`⚙️ Processing directory '${directoryPath}'...`);
+//             const entries: DirectoryEntry[] = [];
+//             const localDirectoryPath = directoryPath.slice(`${outputDirectory}/${id}`.length);
+//             index[localDirectoryPath === '' ? '/' : localDirectoryPath] = entries;
+//             for (const name of names) {
+//                 const itemPath = `${directoryPath}/${name}`;
+//                 try {
+//                     const stats = await fs.stat(itemPath);
+//                     if (stats.isDirectory()) {
+//                         const nextLevelChildren = await fs.readdir(itemPath);
+//                         const folderEntry: DirectoryFolderEntry = { childCount: nextLevelChildren.length, name, typeId: 'folder' };
+//                         entries.push(folderEntry);
+//                         await listDirectoryEntriesRecursively(itemPath, nextLevelChildren);
+//                     } else {
+//                         const objectEntry: DirectoryObjectEntry = { id: nanoid(), lastModifiedAt: stats.mtimeMs, name, size: stats.size, typeId: 'object' };
+//                         entries.push(objectEntry);
+//                     }
+//                 } catch (error) {
+//                     throw new Error(`Unable to get information for '${name}' in 'buildDirectoryIndex'. ${String(error)}`);
+//                 }
+//             }
+//             entries.sort((left, right) => {
+//                 const typeComparison = left.typeId.localeCompare(right.typeId);
+//                 return typeComparison === 0 ? left.name.localeCompare(right.name) : typeComparison;
+//             });
+//         }
 
-        const toplevelNames = await fs.readdir(`public/${id}`);
-        await listDirectoryEntriesRecursively(`public/${id}`, toplevelNames);
-        await fs.writeFile(`./public/${id}Index.json`, JSON.stringify(index), 'utf8');
-        console.info('✅ Public directory index built.');
-    } catch (error) {
-        console.error('❌ Error building public directory index.', error);
-    }
-}
+//         const toplevelNames = await fs.readdir(`${outputDirectory}/${id}`);
+//         await listDirectoryEntriesRecursively(`${outputDirectory}/${id}`, toplevelNames);
+//         await fs.writeFile(`./${outputDirectory}/${id}Index.json`, JSON.stringify(index), 'utf8');
+//         console.info('✅ Directory index built.');
+//     } catch (error) {
+//         console.error('❌ Error building directory index.', error);
+//     }
+// }
 
 export { buildProject, releaseProject, syncProjectWithGitHub, testProject } from '@/operations/manageProject';
 
